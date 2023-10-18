90373751b2767f1914756b4e00f9ef65e0f99302
E146U17 <E146U17@E146PC17.GYMLTS.BID.LOCAL>
2023-10-18 16:33:50 +0200

    123

---
+++ FoerderVerein_1/src/beFördera.java
@@ -0,0 +1,30 @@
+
+public class beFördera extends Fördranträge {
+
+	public beFördera(int summe, String datum, String zahlungsrhythmus,
+			String fachbereich, String name, String gen) {
+		super(summe, datum, zahlungsrhythmus, fachbereich, name, gen);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
