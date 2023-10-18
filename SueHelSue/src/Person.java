11292616a8876f34b946a553ca0855c00ce5276b
E146U06 <E146U06@E146PC06.GYMLTS.BID.LOCAL>
2023-10-18 16:43:21 +0200

    Klassen

---
+++ SueHelSue/src/Person.java
@@ -0,0 +1,34 @@
+
+public abstract class Person {
+	
+	public String fach;
+	public String klasse;
+	public String name;
+	public int Zeit;
+
+	
+	public String getFach() {
+		return fach;
+	}
+	public void setFach(String fach) {
+		this.fach = fach;
+	}
+	public String getKlasse() {
+		return klasse;
+	}
+	public void setKlasse(String klasse) {
+		this.klasse = klasse;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getZeit() {
+		return Zeit;
+	}
+	public void setZeit(int zeit) {
+		Zeit = zeit;
+	}
+}
